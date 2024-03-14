ed0337b46d2c3c0dc86e9d0d4c899740640285b0
Henry <henrylemons9@gmail.com>
2024-03-14 10:09:32 -0700

    Got the values working

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const myFunction = () => {
-  selectValue = 
-  document.getElementById('#Strength');
-  console.log(selectValue  )  
+  strength = document.getElementById('Strength').value;
+  perception = document.getElementById('Perception').value;
+  endurance = document.getElementById('Endurance').value;
+  charisma = document.getElementById('Charisma').value;
+  intellegence = document.getElementById('Intellegence').value;
+  agility = document.getElementById('Luck').value;
+  luck = document.getElementById('Luck').value;
  }
