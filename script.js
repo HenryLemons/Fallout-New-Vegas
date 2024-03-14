d8fa830e83b9ae672cfbfb9dd9c0ba9eed263ec0
Henry <henrylemons9@gmail.com>
2024-03-14 10:17:47 -0700

    Got it working has bugs with multiple outputs

--- script.js
+++ script.js
@@ -5,5 +5,11 @@ const myFunction = () => {
   charisma = document.getElementById('Charisma').value;
   intellegence = document.getElementById('Intellegence').value;
   agility = document.getElementById('Agility').value;
-  luck = document.getElementById('Luck').value;
+
+  document.getElementById("Skill_Strength").innerHTML = (strength * 2) + 6;
+  document.getElementById("Skill_Perception").innerHTML = (perception * 2) + 6;
+  document.getElementById("Skill_Endurance").innerHTML = (endurance * 2) + 6;
+  document.getElementById("Skill_Charisma").innerHTML = (charisma * 2) + 6;
+  document.getElementById("Skill_Intelligence").innerHTML = (intellegence * 2) + 6;
+  document.getElementById("Skill_Agility").innerHTML = (agility * 2) + 6;
  }
